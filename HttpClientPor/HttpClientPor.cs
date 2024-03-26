cc6d189ba0a941d70d90bed383b072c9da77d5fc
longUseWinform <875194705@qq.com>
2024-03-26 18:04:34 +0800

    添加项目文件。

---
+++ HttpClientPor/HttpClientPor.cs
@@ -0,0 +1,37 @@
+﻿using IHttpClient;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientPor
+{
+    [Export(typeof(IHttpClientPor))]
+    public class HttpClientPor: IHttpClientPor
+    {
+        ServiceCollection serviceCollection = new ServiceCollection();
+        IHttpClientFactory httpClientFactory = null;
+        public  HttpClientPor(List<NameWithUri> CreareMessage)
+        {
+            foreach (var createMsg in CreareMessage)
+            {
+                if (createMsg.Uri==null)
+                {
+                    serviceCollection.AddHttpClient(createMsg.Name);
+                    continue;
+                }
+                serviceCollection.AddHttpClient(createMsg.Name, (x) => { x.BaseAddress = createMsg.Uri;  });
+            }
+            ServiceProvider service = serviceCollection.BuildServiceProvider();
+            httpClientFactory = service.GetRequiredService<IHttpClientFactory>();
+            //创建
+        }
+        public IHttpClientFactory GetHttpClientFactory()
+        {
+            return this.httpClientFactory;
+        }
+    }
+}
---
+++ IHttpClient/NameWithUri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHttpClient
+{
+    public class NameWithUri
+    {
+        public string Name { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
